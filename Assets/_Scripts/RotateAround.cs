ef08fe4ca95de689251790d7620edfb5247fe17f
Jonathan Gresl <jgresl@hotmail.com>
2024-09-23 16:59:57 -0600

    Added script to rotate around another object

---
+++ Assets/_Scripts/RotateAround.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour
+{
+    public float Speed;
+    public Transform Target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(Target.position, Vector3.up, Speed * Time.deltaTime);
+    }
+}
